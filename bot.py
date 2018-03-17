31872afae6c58dbcd85ab400a0b008bcd8a32cfa
Thiago Vinicius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2018-03-16 23:39:45 -0300

    Suporte fraco e ruim para download de sticker sets

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import telegram.ext
 from telegram.ext.dispatcher import run_async
 import logging
 import de_nada
+import stickers
 
 SEND_MESSAGE_TIMEOUT = 30
+STICKER_TIMEOUT = 120
 
 def load_file(filename):
     with open(filename, 'r') as file_content:
@@ -20,12 +22,17 @@ def load_allowed_groups():
 
 def register_handlers(dispatcher, masters=[], allowed_groups=[]):
     MessageHandler = telegram.ext.MessageHandler
+    dispatcher.add_handler(MessageHandler(chat_with_sticker_filter(), chat_with_sticker))
     dispatcher.add_handler(MessageHandler(chat_from_master_filter(masters), chat_from_master))
     dispatcher.add_handler(MessageHandler(telegram.ext.Filters.private, chat_from_strangers))
     dispatcher.add_handler(MessageHandler(allowed_group_filter(allowed_groups), update_on_allowed_group))
     dispatcher.add_handler(MessageHandler(telegram.ext.Filters.group, strange_group_update))
     dispatcher.add_handler(MessageHandler(telegram.ext.Filters.all, unhandled_update))
     dispatcher.add_error_handler(error_callback)
+
+def chat_with_sticker_filter():
+    Filters = telegram.ext.Filters
+    return Filters.private & Filters.sticker
 
 def chat_from_master_filter(masters):
     Filters = telegram.ext.Filters
@@ -38,6 +45,43 @@ def allowed_group_filter(allowed_groups):
     chat = Filters.chat(chat_id=allowed_groups)
     group = Filters.group
     return chat & group
+
+def chat_with_sticker(bot, update):
+    set_name = update.message.sticker.set_name
+    print('Recebi um sticker.')
+    if set_name:
+        async_send_message(
+            bot,
+            chat_id=update.message.chat_id,
+            reply_to_message_id=update.message.message_id,
+            text='Guentae que estou trabalhando nisso',
+            timeout=SEND_MESSAGE_TIMEOUT)
+        print('Sticker faz parte de um set, fornecendo set')
+        try:
+            stickers.provide_sticker_pack(
+                bot,
+                update.message.chat_id,
+                set_name,
+                timeout=STICKER_TIMEOUT)
+        except:
+            async_send_message(
+                bot,
+                chat_id=update.message.chat_id,
+                reply_to_message_id=update.message.message_id,
+                text='Falhei mizeravelmente. Desculpe',
+                timeout=SEND_MESSAGE_TIMEOUT)
+    else:
+        print('Sticker não faz parte de um set')
+        async_send_message(
+            bot,
+            chat_id=update.message.chat_id,
+            reply_to_message_id=update.message.message_id,
+            text='Oops, não sei o que fazer com esse sticker',
+            timeout=SEND_MESSAGE_TIMEOUT)
+
+@run_async
+def async_send_message(bot, *args, **kwargs):
+    bot.send_message(*args, **kwargs)
 
 @run_async
 def chat_from_master(bot, update):
---
+++ stickers.py
@@ -0,0 +1,74 @@
+import zipfile
+import os.path
+import io
+import hashlib
+
+def _download_sticker_set(bot, set_id, timeout):
+    packed_output = io.BytesIO()
+    stickerset = bot.get_sticker_set(set_id)
+    with zipfile.ZipFile(packed_output, mode='w') as zip_output:
+        for sticker in stickerset.stickers:
+            sticker_file = bot.get_file(sticker.file_id, timeout=timeout)
+            sticker_img = sticker_file.download_as_bytearray() # deveria ter timeout
+            path_in_zip = os.path.basename(sticker_file.file_path)
+            zip_output.writestr(path_in_zip, sticker_img)
+    return packed_output.getvalue()
+
+def _upload_and_send(bot, chat_id, name, contents, timeout):
+    print('enviando novo')
+    stream = io.BytesIO(contents)
+    sent = bot.send_document(
+        chat_id=chat_id,
+        document=stream,
+        filename=name,
+        timeout=timeout)
+    return sent.document.file_id
+
+def _send_existing(bot, chat_id, file_id):
+    print('enviando existente')
+    bot.send_document(chat_id=chat_id, document=file_id)
+
+def _upload_id_filename(set_id):
+    digest = hashlib.sha256(bytes(set_id, 'utf-8')).hexdigest()
+    return os.path.join('sticker_set_db', digest)
+
+def _store_upload_id(set_id, file_id):
+    file_name = _upload_id_filename(set_id)
+    with open(file_name, 'x') as storage:
+        storage.write(file_id)
+    with open('%s_name' % file_name, 'x') as storage:
+        # para debug
+        storage.write(set_id)
+
+def _get_upload_id(set_id):
+    file_name = _upload_id_filename(set_id)
+    try:
+        with open(file_name, 'r') as storage:
+            return storage.read()
+    except:
+        return None
+
+def provide_sticker_pack(bot, chat_id, set_id, timeout=None):
+    upload_id = _get_upload_id(set_id)
+    if upload_id:
+        _send_existing(bot, chat_id, upload_id)
+    else:
+        pack = _download_sticker_set(bot, set_id, timeout)
+        filename = '%s.zip' % set_id
+        upload_id = _upload_and_send(bot, chat_id, filename, pack, timeout)
+        _store_upload_id(set_id, upload_id)
+
+if __name__ == '__main__':
+    import telegram
+    with open('bot.token') as token:
+        bot = telegram.Bot(token.readlines()[0].strip())
+    #zipado = download_sticker_set(bot, 'avioesemusicas')
+    #with open('zipado.zip', 'wb') as saida:
+    #    saida.write(zipado)
+
+    upload_id = get_upload_id('teste')
+    if upload_id:
+        send_existing(bot, upload_id)
+    else:
+        upload_id = upload_and_send(bot, 'teste.txt', b'isso eh um teste')
+        store_upload_id('teste', upload_id)
